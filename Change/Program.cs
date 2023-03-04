6999b0d8340223ca0af70025eb304b12d0f9ec6a
arturvardanyan442@gmail.com <arturvardanyan442@gmail.com>
2023-03-04 19:02:23 +0400

    Add project files.

---
+++ Change/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Change
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Dollar = ");
+            int sum = int.Parse(Console.ReadLine());
+            int kurs = 389;
+            ChangeClass changeClass = new ChangeClass();
+            int k = changeClass.Poxanakum(sum, kurs);
+            Console.Write("Dram = ");
+            Console.WriteLine(k);
+            Tpel tpel = new Tpel();
+            tpel.Tpell(k);
+
+        }
+    }
+}
---
+++ Change/Tpel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change
+{
+    internal class Tpel
+    {
+        public void Tpell(int k)
+        {
+            int[] x = { 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10 };
+            for (int i = 0; i < x.Length; i++)
+            {
+                int z = k / x[i];
+                if (z > 0)
+                    Console.WriteLine(z + "*" + x[i]);
+                k = k - (z * x[i]);
+            }
+            
+        }
+    }
+}
